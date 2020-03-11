f37fc69aee90a06bc6b13befe5676d9b6b595055
Alan Marx <alanmarx@hotmail.com>
2020-03-11 23:07:41 +0000

    created a client.js file for my snake project

--- play.js
+++ play.js
@@ -1,18 +1,5 @@
-const net = require('net');
 
-const connect = () => {
-  const connection = net.createConnection({
-      host: '192.168.88.177',
-      port: 50541
-    });  
-  connection.setEncoding('utf8');
-  
-  connection.on('data', (data) => {
-    console.log(data);
-  });
-
-  return connection;
-};
+const { connect } = require('./client');
 
 console.log('Connecting...');
 connect();

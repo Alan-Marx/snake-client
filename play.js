9691b9a63ab1f1fe698716f9766d41a9bcc4a766
Alan Marx <alanmarx@hotmail.com>
2020-03-11 22:58:59 +0000

    started my snake program

---
+++ play.js
@@ -0,0 +1,18 @@
+const net = require('net');
+
+const connect = () => {
+  const connection = net.createConnection({
+      host: '192.168.88.177',
+      port: 50541
+    });  
+  connection.setEncoding('utf8');
+  
+  connection.on('data', (data) => {
+    console.log(data);
+  });
+
+  return connection;
+};
+
+console.log('Connecting...');
+connect();
